918186b1e1802e9cf7385bd90c42a271a017088e
balu <admin@juniq.pw>
2020-05-10 01:25:06 +0200

    Added onEnable and onDisable

--- src/pw/juniq/main/Main.java
+++ src/pw/juniq/main/Main.java
@@ -1,4 +1,16 @@
 package pw.juniq.main;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    private static String version = "1.0";
+    public void onEnable() {
+        System.out.print("System Version " + version + " startet...");
+    }
+
+    public void onDisable() {
+        System.out.print("System Version " + version + " stoppt...");
+    }
+
+
 }
